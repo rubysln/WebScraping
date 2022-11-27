4834335ceff1ae20e83d3e8a300a58c9030e1ff0
rubysln <prostosekundu48@gmail.com>
2022-11-27 15:27:50 +0300

    Добавлен функционал парсинга всех отзывов с конкретного магазина

--- src/Scrapping.java
+++ src/Scrapping.java
@@ -5,15 +5,19 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class Scrapping {
     public static void doScrapping() throws IOException, IndexOutOfBoundsException {
         List<Integer> numberOfShop = new ArrayList<>();
         Document docMain = Jsoup.connect("https://nanegative.ru/internet-magaziny").get();
+        HashMap<Integer, String> mapShops = new HashMap<>();
         System.out.println(docMain.title());
 
-        for(int i = 1; i <= 11; i++){
+        Elements pagesCount = docMain.selectXpath("//a[starts-with(@href,'/internet-magaziny?page=')]");
+        for(int i = 1; i <= pagesCount.size(); i++){
             Document doc = Jsoup.connect("https://nanegative.ru/internet-magaziny?page=" + i).get();
             Elements postTitleElement = doc.getElementsByAttributeValue("class", "ss");
             Elements postRateElement = doc.getElementsByAttributeValue("class", "sro");
@@ -23,8 +27,37 @@ public class Scrapping {
                 Element rateCountElement = postRateCountElement.get(j);
                 Element titleElement = postTitleElement.get(j);
                 Element rateElement = postRateElement.get(j);
-                System.out.println((numberOfShop.size()) + ") " +titleElement.text() + " | " + rateElement.text() + " | " + rateCountElement.text());
+                String str = titleElement.text().replace("Отзывы о", "");
+                System.out.println((numberOfShop.size()) + ") " + str + " | " + rateElement.text() + " | " + rateCountElement.text());
                 numberOfShop.add(1);
+                mapShops.put(j, titleElement.attr("href"));
+            }
+        }
+
+        System.out.println("Введите номер интересующего магазина: ");
+        Scanner scanner = new Scanner(System.in);
+        int inputShop = scanner.nextInt();
+
+        docMain = Jsoup.connect("https://nanegative.ru" + mapShops.get(inputShop)).get();
+        var pages = docMain.selectXpath("//a[starts-with(@href,'" + mapShops.get(inputShop) + "?page=')]");
+        if(pages.size() == 0){
+            Elements reviews = docMain.getElementsByAttributeValue("itemprop", "description");
+            if(reviews.size() == 0){
+                System.out.println("Отзывы о данном магазине отсутствуют!");
+            }
+            else{
+                for(var e: reviews){
+                    System.out.println(e.text());
+                }
+            }
+        }
+        else{
+            for(int i = 0; i < pages.size(); i++){
+                docMain = Jsoup.connect("https://nanegative.ru" + mapShops.get(inputShop) + "?page=" + i).get();
+                Elements reviews = docMain.getElementsByAttributeValue("itemprop", "description");
+                for(var e: reviews){
+                    System.out.println(e.text());
+                }
             }
         }
     }
